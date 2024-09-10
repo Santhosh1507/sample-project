3157f42e5aa32ca7e520bdb6b2bbe15b5d04bf8d
Santhosh1507 <webdesign1507@gmail.com>
2024-09-10 19:07:07 +0530

    first commit

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connect = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect('mongodb+srv://santhoshmce1234:Santhosh2024@cluster0.q5q3e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
         console.log('Database connected successfully');
     } catch (error) {
         console.log('Database connection failed');
